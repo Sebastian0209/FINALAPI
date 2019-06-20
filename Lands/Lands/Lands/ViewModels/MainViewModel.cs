ab3220149c84336d8543aff2acfbe711641346cb
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-06-20 18:12:04 -0400

    Commit Interface y iniciando login view models

---
+++ Lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+
+        }
+        #endregion
+    }
+}
