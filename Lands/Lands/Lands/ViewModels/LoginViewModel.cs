a3832b8fdfa270f1d32e88452f60a119507d7521
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-06-20 22:14:11 -0400

    BaseViewModel con refresh

--- Lands/Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -5,26 +5,110 @@ using System.Text;
 
 namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
-    class LoginViewModel
+    using Xamarin.Forms;
+
+    class LoginViewModel : BaseViewModel
     {
+ 
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+
+        #endregion
         #region Properties
 
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsRemembered { get; set; }
+        public string Email
+        {
+            get; set;
+        }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+        public bool IsRemembered
+        {
+            get; set;
+        }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
 
         #endregion
 
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Introduzca su correo",
+                    "Aceptar");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Introduzca su password",
+                    "Aceptar");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "carlos68@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "Error",
+                   "Email o Password incorrectos",
+                   "Aceptar");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                   "Ok",
+                   "Aceptado",
+                   "Aceptar");
+
+
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
-
+            this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
     }
